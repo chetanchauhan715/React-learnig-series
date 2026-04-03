2d6263326f4549e99c00e79778cffe205eb40a40
Chetan Chauhan <chetan99256@gmail.com>
2026-04-03 21:36:13 +0530

    Initial React project setup

---
+++ Basic_React/01Basics/src/App.jsx
@@ -0,0 +1,12 @@
+import Name from './name.jsx' ;
+
+function App() {
+
+  return (
+    // <h1>Chetan Chauhan | vite </h1>
+    <Name/>
+
+  )
+}
+
+export default App
---
+++ Basic_React/01Basics/src/name.jsx
@@ -0,0 +1,7 @@
+function Name(){
+    return (
+    <h2>Chetan Chauhan | 2021</h2>
+    )
+}
+
+export default Name;
