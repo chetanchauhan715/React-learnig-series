53ba841ebccab8b7c5dc7c728d5e343c07e890da
Chetan Chauhan <chetan99256@gmail.com>
2026-05-07 16:51:35 +0530

    ADD: clear all functionality & prettier extention add to vs code for code formating

--- practice/src/components/notes.jsx
+++ practice/src/components/notes.jsx
@@ -1,81 +1,66 @@
 import { useState } from "react";
 
-export default function NotesApp(){
+export default function NotesApp() {
+  const [input, setInput] = useState("");
+  const [notes, setNotes] = useState([]);
 
-    const [input , setInput] = useState("");
-    const [notes , setNotes] = useState([]);
-
-    function add(){
-        if(input.trim() === ""){
-            alert("Please Enter Valid Note");
-            return;
-        }
-
-        const newNote = {
-            id:Date.now(),
-            Text:input
-        }
-
-        setNotes( (prev) => [...prev , newNote]);
-
-       setInput("");
+  function add() {
+    if (input.trim() === "") {
+      alert("Please Enter Valid Note");
+      return;
     }
 
-    function deleteNote(id){
-        const updatedNotes = notes.filter(
-            (note) => note.id !== id
-        );
+    const newNote = {
+      id: Date.now(),
+      text: input,
+    };
 
-        setNotes(updatedNotes);
-    }
+    setNotes((prev) => [...prev, newNote]);
 
-   function clearAll(){
-    notes = [];
-   }
+    setInput("");
+  }
 
+  function deleteNote(id) {
+    const updatedNotes = notes.filter((note) => note.id !== id);
 
+    setNotes(updatedNotes);
+  }
 
+  function clearAll() {
+    setNotes([]);
+  }
 
-    return (
-        <div style={ {border:"2px solid white" , padding:"20px"}}>
+  return (
+    <div style={{ border: "2px solid white", padding: "20px" }}>
+      <div>
+        <input
+          type="text"
+          placeholder="Enter Your Note here ..."
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
 
-            <div>
-                <input 
-                type="text"
-                placeholder="Enter Your Note here ..."
-                value={input}
-                onChange={ (e)=> setInput(e.target.value)}
-                />
+        <button onClick={add}>Add</button>
+        <br />
+        {notes.length > 0 && <button onClick={clearAll}>Clear All</button>}
 
-                <button onClick={add}>
-                    Add
-                    </button>
+        <p>Total Notes : {notes.length}</p>
+      </div>
 
-                    <p>Total Notes :  {notes.length}</p>
-
-                    <button onClick={clearAll}>Clear All</button>
-            </div>
-
-            <div>
-                <ul>
-                    {
-                    notes.length === 0 ?
-
-                    (<li>No notes yet</li>) :
-                    
-                    (notes.map( (note) => (
-                        <li style={ {listStyle:"none"}} key={note.id}>
-                            {note.Text}
-                            <button onClick={()=> deleteNote(note.id)}>
-                                Delete
-                            </button>
-                        </li>
-                    )))
-
-                }
-                </ul>
-            </div>
-
-        </div>
-    )
+      <div>
+        <ul>
+          {notes.length === 0 ? (
+            <li>No notes yet</li>
+          ) : (
+            notes.map((note) => (
+              <li style={{ listStyle: "none" }} key={note.id}>
+                {note.text}
+                <button onClick={() => deleteNote(note.id)}>Delete</button>
+              </li>
+            ))
+          )}
+        </ul>
+      </div>
+    </div>
+  );
 }
